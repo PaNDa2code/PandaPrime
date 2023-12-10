e983b1dc53770bbabd841b87f8c94945a06c3961
PaNDa2Code <moaaz0688@gmail.com>
2023-12-09 21:00:34 -0500

    initial commit

---
+++ PandaPrimes.c
@@ -0,0 +1,104 @@
+#include <Python.h>
+#include <primesieve.h>
+
+typedef struct
+{
+    PyObject_HEAD
+        primesieve_iterator it;
+    size_t start, end;
+} primes_range;
+
+static PyObject *PrimeGen_new(PyTypeObject *type, PyObject *args, PyObject *kwarg)
+{
+    int start, end;
+
+    if (!PyArg_ParseTuple(args, "ii", &start, &end))
+    {
+        return NULL;
+    }
+
+    primes_range *gen = (primes_range *)type->tp_alloc(type, 0);
+
+    if (!gen)
+    {
+        return NULL;
+    }
+
+    gen->start = (size_t)start;
+    gen->end = (size_t)end;
+    primesieve_init(&gen->it);
+
+    return (PyObject *)gen;
+}
+
+static void PrimeGen_dealloc(primes_range *gen)
+{
+    primesieve_clear(&gen->it);
+    Py_TYPE(gen)->tp_free((PyObject *)gen);
+}
+
+static PyObject *PrimeGen_next(primes_range *gen)
+{
+    size_t prime = primesieve_next_prime(&gen->it);
+    if (prime <= gen->end)
+    {
+        return Py_BuildValue("n", prime);
+    }
+    else
+    {
+        PyErr_SetNone(PyExc_StopIteration);
+        return NULL;
+    }
+}
+
+static PyObject *PrimeGen_iter(primes_range *gen)
+{
+    Py_INCREF(gen);
+    return (PyObject *)gen;
+}
+
+static PyMethodDef PrimeGen_methods[] = {
+    {"next_prime", (PyCFunction)PrimeGen_next, METH_NOARGS, "Get the next prime in the range."},
+    {NULL, NULL, 0, NULL}};
+
+static PyTypeObject PrimeGenType = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+        .tp_name = "primes.primes_range",
+    .tp_basicsize = sizeof(primes_range),
+    .tp_itemsize = 0,
+    .tp_dealloc = (destructor)PrimeGen_dealloc,
+    .tp_flags = Py_TPFLAGS_DEFAULT,
+    .tp_doc = "primes_range objects",
+    .tp_iter = (getiterfunc)PrimeGen_iter,
+    .tp_iternext = (iternextfunc)PrimeGen_next,
+    .tp_methods = PrimeGen_methods,
+    .tp_new = PrimeGen_new,
+};
+
+static PyModuleDef primes_module = {
+    PyModuleDef_HEAD_INIT,
+    "PaNDaPrime",
+    NULL,
+    -1,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL};
+
+PyMODINIT_FUNC PyInit_PandaPrimes(void)
+{
+    PyObject *module;
+    module = PyModule_Create(&primes_module);
+
+    if (module == NULL)
+        return NULL;
+
+    if (PyType_Ready(&PrimeGenType) < 0)
+        return NULL;
+
+    Py_INCREF(&PrimeGenType);
+    PyModule_AddObject(module, "primes_range", (PyObject *)&PrimeGenType);
+
+    return module;
+}
