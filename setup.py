845f7258869a6935e64a8c6957e1a65eee9b76c6
PaNDa2Code <moaaz0688@gmail.com>
2023-12-14 22:31:14 -0500

    chore: reamove not needed `CustomInstall` and `CustomBuild_ext`

--- setup.py
+++ setup.py
@@ -1,27 +1,13 @@
 from setuptools import Extension, setup
-from setuptools.command.install import install
 from setuptools.command.build import build
-from setuptools.command.build_ext import build_ext
 import subprocess
 
-# Download the header files before installing or building
-class CustomInstall(install):
-    def run(self):
-        print("The custom install is running")
-        # Run your script here before the installation
-        subprocess.run(['python', 'post_setup.py'])
-        # Continue with the default installation process
-        install.run(self)
+# Download the header files before building
 class CustomBuild(build):
     def run(self):
         print("The custom bulid is running")
         subprocess.run(['python', 'post_setup.py'])
         build.run(self)
-class CustomBuild_ext(build_ext):
-    def run(self):
-        print("The custom bulid_ext is running")
-        subprocess.run(['python', 'post_setup.py'])
-        build_ext.run(self)
 
 setup(
     setup_requires=['setuptools','requests'],
@@ -35,7 +21,5 @@ setup(
     ],
     cmdclass={
         'build':CustomBuild,
-        'install':CustomInstall,
-        'build_ext':CustomBuild_ext,
         }
 )
