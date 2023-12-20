09d65b0f39c8587fa378e1897aa07b4d118bedb7
PaNDa2Code <moaaz0688@gmail.com>
2023-12-19 22:12:41 -0500

    feat: add `generate_primes` fucntion and made tests

--- PandaPrimes/src/PandaPrimes.c
+++ PandaPrimes/src/PandaPrimes.c
@@ -186,24 +186,24 @@ typedef struct
 } Pint;
 
 Pint get_int_types(u_int64_t integer)
-{   
+{
     Pint m;
-    if (integer <= 65535)
+    if (integer <= UINT16_MAX)
     {
         m.numpy_int_type = NPY_UINT16;
         m.primesieve_int_type = UINT16_PRIMES;
     }
-    else if (integer <= 4294967295)
+    else if (integer <= UINT32_MAX)
     {
         m.numpy_int_type = NPY_UINT32;
         m.primesieve_int_type = UINT32_PRIMES;
     }
-    else if (integer <= 18446744073709551615)
+    else if (integer <= UINT64_MAX)
     {
         m.numpy_int_type = NPY_UINT64;
         m.primesieve_int_type = UINT64_PRIMES;
     }
-    return m;  
+    return m;
 };
 
 static PyObject *generate_primes(PyObject *self, PyObject *args)
@@ -264,10 +264,65 @@ static PyObject *generate_primes(PyObject *self, PyObject *args)
     return array;
 };
 
+static PyObject *generate_n_primes(PyObject *self, PyObject *args)
+{
+    PyGILState_STATE gstate = PyGILState_Ensure();
+
+    u_int64_t start;
+    size_t n;
+    if (PyTuple_Size(args) == 1)
+    {
+        start = 2;
+        if (!PyArg_ParseTuple(args, "n", &n))
+        {
+            PyErr_SetString(PyExc_TypeError, "Invalid argument ==> arguments should be int type");
+            PyGILState_Release(gstate);
+            return NULL;
+        }
+    }
+    else if (PyTuple_Size(args) == 2)
+    {
+        if (!PyArg_ParseTuple(args, "nK", &n, &start))
+        {
+            PyErr_SetString(PyExc_TypeError, "Invalid arguments ==> arguments should be int type");
+            PyGILState_Release(gstate);
+            return NULL;
+        }
+    }
+    else
+    {
+        PyErr_SetString(PyExc_TypeError, "Invalid number of arguments");
+        PyGILState_Release(gstate);
+        return NULL;
+    }
+
+
+    void *primes = primesieve_generate_n_primes(n, start, UINT64_PRIMES);
+
+    npy_intp dims[1] = {n};
+
+    PyObject *array = PyArray_SimpleNewFromData(1, dims, NPY_UINT64, primes);
+
+    if (array == NULL)
+    {   
+        PyErr_SetString(PyExc_MemoryError, "Failed to create NumPy array");
+        PyGILState_Release(gstate);
+        return NULL;
+    }
+    
+    // make numpy to free the memory in the array for you when it's garbage collected
+    PyArray_ENABLEFLAGS((PyArrayObject *)array, NPY_ARRAY_OWNDATA);
+
+    // never forget these also
+
+    PyGILState_Release(gstate);
+    return array;
+}
 // Module Initialization
 
 static PyMethodDef PandaPrimes_methods[] = {
     {"generate_primes", (PyCFunction)generate_primes, METH_VARARGS, "generate numpy array of primes"},
+    {"generate_n_primes", (PyCFunction)generate_n_primes, METH_VARARGS, "generate numpy array of primes"},
     {NULL, NULL, 0, NULL}};
 
 static PyModuleDef PandaPrimes_module = {
